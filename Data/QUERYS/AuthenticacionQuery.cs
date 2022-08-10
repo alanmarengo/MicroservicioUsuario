2738d815f75ed7ef55e6550ae56a4994050a412a
alanmarengo98 <alanmarengo98@gmail.com>
2022-08-09 21:57:23 -0300

    RFT USER MIC

--- Data/QUERYS/AuthenticacionQuery.cs
+++ Data/QUERYS/AuthenticacionQuery.cs
@@ -33,7 +33,7 @@ namespace Data.QUERYS
             //Inicializo la app de firebase aca 
             FirebaseApp.Create(new AppOptions()
             {
-                Credential = GoogleCredential.FromFile(@"C:\Users\Alan\Pictures\INFORME METODOLOGIA\MicroservicioUser\MicroservicioUser\Privatekey\usuario-5093e-firebase-adminsdk-43fcf-1bf89503c6.json"),
+                Credential = GoogleCredential.FromFile(@"C:\Users\alanm\Documents\PS\Pagina-web-Proyecto-final-master\Repositorio Microservicios\MicroservicioUsuario\MicroservicioUser\Privatekey\usuario-5093e-firebase-adminsdk-43fcf-1bf89503c6.json"),
             });
             var result = await FirebaseAuth.DefaultInstance.GetUserAsync(uids);
             var ExisteCliente = (from x in context.user where x.uid == uids select x.Id).FirstOrDefault<int>();
@@ -84,7 +84,7 @@ namespace Data.QUERYS
             //Inicializo la app de firebase aca 
             FirebaseApp.Create(new AppOptions()
             {
-                Credential = GoogleCredential.FromFile(@"C:\Users\Alan\Pictures\INFORME METODOLOGIA\MicroservicioUser\MicroservicioUser\Privatekey\usuario-5093e-firebase-adminsdk-43fcf-1bf89503c6.json"),
+                Credential = GoogleCredential.FromFile(@"C:\Users\alanm\Documents\PS\Pagina-web-Proyecto-final-master\Repositorio Microservicios\MicroservicioUsuario\MicroservicioUser\Privatekey\usuario-5093e-firebase-adminsdk-43fcf-1bf89503c6.json"),
             });
             var result = await FirebaseAuth.DefaultInstance.GetUserAsync(uids);
             var ExisteCliente = (from x in context.user where x.uid == uids select x.Id).FirstOrDefault<int>();
